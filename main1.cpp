3fe200224f298579e77c44aeb9fa445f0d3b4614
KapitanMarbel <marbellaaustingyan@gmail.com>
2021-03-11 15:25:29 +0800

    added division

---
+++ main1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout<< "Please enter a number: ";
+	cin>> a;
+	
+	cout<< "Please enter a number: ";
+	cin>> b;
+	
+	c = a/b;
+	
+	cout<< "The quotient is: " << c << endl;
+}
