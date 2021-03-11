6f17aa31d6c4327fd8a5ae044a99f7c44f177626
Paturiko <luizdalicano@gmail.com>
2021-03-11 15:25:25 +0800

    Addition Program

---
+++ Addition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int a;
+	int b;
+	int c;
+	cout<<"Enter your first number: ";
+	cin>>a;
+	cout<<endl;
+	cout<<"Enter your second number: ";
+	cin>>b;
+	cout<<endl;
+	c = a + b;
+	cout<<"The sum is: "<<c;
+	
+	return 0;
+}
