5be4f39eedbbd1310943f36537a365bba2c59cd4
Nelliosis <thewest105@yahoo.com>
2021-05-31 22:35:49 +0800

    refamiliarizing with git commands

--- main.cpp
+++ main.cpp
@@ -1,108 +1,61 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <list>
+#include <cmath>
 
 using namespace std;
 
-struct MovieNode
-{
-  int
-        id,
-        copies;
-
-  std::string
-        title,
-        genre,
-        prod,
-        location;
-
-};
-
 int main()
 {
+  int stack = 0;
+  //int weight = 0;
+  int coins = 0;
+  int Weight_Catcher = 0;
 
-MovieNode node;
-list<MovieNode> MovieList;
-list<MovieNode>::iterator iter;
+  cout << "Enter number of stacks to weigh: ";
+  cin >> stack;
+
+  cout << "Enter the number of coins in the stack: ";
+  cin >> coins;
+
+  int Weight_Containter[stack][coins];
 
 
-  string line;
-    ifstream myfile ("/Users/chikashoki/Documents/GitHub/Hello-Git/MovieList.txt");
-    if (myfile.is_open())
+for(int i = 0; i < stack; i++) //assigning weights
+{
+
+  for(int j = 0; j < coins; j++)
+  {
+    cout << "Stack " << i;
+    cout << " Coin " << j << ": ";
+    cin >> Weight_Catcher;
+    Weight_Containter[i][j] = Weight_Catcher;
+
+  }
+}
+cout << endl;
+for(int i = 0; i < stack; i++) //determining weights
+{
+
+  for(int j = 0; j < coins; j++)
+  {
+    cout << "Stack: " << i;
+    cout << " Coin " << j << ": ";
+
+    Weight_Catcher = Weight_Containter[i][j];
+
+    if(Weight_Catcher % 10 == 0)
     {
-      int count = 0;
-      while ( getline (myfile,line) )
-      {
-        switch(count)
-        {
-          case 0:
-            node.id = stoi(line);
-            break;
-          case 1:
-            node.title = line;
-            break;
-          case 2:
-          node.genre = line;
-            break;
-          case 3:
-          node.prod = line;
-            break;
-          case 4:
-          node.copies = stoi(line);
-            break;
-          case 5:
-          //add location
-            break;
-          default:
-            break;
-        }
-
-        if(count < 5)
-        {
-          count++;
-        }
-        else
-        {
-          count = 0;
-          MovieList.push_back(node);
-        }
-
-      }
-      myfile.close();
-
+      cout << "is genuine" << endl;
+    }
+    else
+    {
+      cout << "is fake" << endl;
     }
 
-    else cout << "Unable to open file" << endl;
-
-    for(iter = MovieList.begin(); iter != MovieList.end(); iter++)
-    {
-        cout << "ID: " << iter->id << endl;
-        cout << "Copies: " << iter->copies << endl;
-        cout << "Title: " << iter->title << endl;
-        cout << "Genre: " << iter->genre << endl;
-        cout << "Production: " << iter->prod << endl;
-        cout << endl;
-
-    }
-
-//cout << node.id << endl << node.title << endl << node.genre << endl << node.prod << endl << node.copies << endl;
-  return 0;
+  }
 }
 
 
-//system("open /System/Applications/Preview.app /Users/chikashoki/Documents/GitHub/Hello-Git/sched.png");
-//system("start C:\\Users\\marbe\\Desktop\\graphics.h_test\\ImageGlass_8.0.12.8_x64\\ImageGlass.exe ");
 
 
-/* string filename = "/Users/chikashoki/Documents/GitHub/Hello-Git/sched.png";
-string cmd("open /System/Applications/Preview.app ");
-  cmd += filename;
-system(cmd.c_str());
-
-#if _WIN32
-        string filename = "C:\\Users\\marbe\\Desktop\\graphics.h_test\\1.jpg";
-        #else
-        string filename = "/Users/chikashoki/Documents/GitHub/Hello-Git/sched.png";
-        #endif
-*/
+return 0;
+}
